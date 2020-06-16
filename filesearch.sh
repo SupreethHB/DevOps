1f5e2fb72899fcadc69fa41d583a4fe690e4899e
H B <shb@its.jnj.com>
2020-06-16 21:56:30 +0530

    commit newly created file

---
+++ filesearch.sh
@@ -0,0 +1,35 @@
+if [ $# = 2 ]
+then
+	echo "Entered expected arguments"
+	if [ -d $1 ]
+	then 
+		echo "directory exists"
+
+
+if [ -d $1/.git ]
+then
+	echo "$1 is valid git repository path"
+	var = `git branch --list $2 | grep $2`
+	if [ $var = 0 ]
+	then
+		echo "Branch $2 already exists"
+	else
+		git branch $2
+		echo"Branch $2 is created"
+		git checkout $2
+		echo "New file created in $2  branch" > file02.txt
+		git add .
+		git commit -m "commit newly created file"
+		git push
+	fi
+else
+	echo "Please enter valid git repository, enter $1 path is incorrect"
+	fi
+else
+	echo "$1 directory doesnot exists"
+fi
+else 
+	exit 1
+fi
+
+
